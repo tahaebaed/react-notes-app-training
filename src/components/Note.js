3233d6b56d345e9a7d14aa99ae56a1742064a3ea
Taha Ebaed <tahaebaed2@gmail.com>
2022-06-01 22:12:06 +0200

    add [edit-icon] to the note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,19 +1,20 @@
-import { MdDeleteForever } from 'react-icons/md';
+import { MdDeleteForever, MdModeEditOutline } from 'react-icons/md';
 
 const Note = ({ id, text, date, handleDeleteNote }) => {
-	return (
-		<div className='note'>
-			<span>{text}</span>
-			<div className='note-footer'>
-				<small>{date}</small>
-				<MdDeleteForever
-					onClick={() => handleDeleteNote(id)}
-					className='delete-icon'
-					size='1.3em'
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className='note'>
+      <span>{text}</span>
+      <div className='note-footer'>
+        <small>{date}</small>
+        <MdModeEditOutline className='edit-icon' size='1.3em' />
+        <MdDeleteForever
+          onClick={() => handleDeleteNote(id)}
+          className='delete-icon'
+          size='1.3em'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Note;
